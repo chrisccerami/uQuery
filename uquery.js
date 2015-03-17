a17330b23e13c8d933be4fa2cf03e5c5e2680dac
Chris C Cerami <chrisccerami@gmail.com>
2015-03-17 17:10:04 -0400

    Implement makeRed function

--- uquery.js
+++ uquery.js
@@ -4,3 +4,10 @@ function makeBold(selector) {
     target[i].style.fontWeight = "bold";
   };
 }
+
+function makeRed(selector) {
+  var target = document.getElementsByClassName(selector);
+  for (var i = 0, ii = target.length; i < ii; i++) {
+    target[i].style.color = "red";
+  };
+}
